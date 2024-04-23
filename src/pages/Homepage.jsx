feac94992ef8a155d1f33c8a8e2723f1dc828f45
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-23 22:14:36 +0200

    navbar added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <div>WORLDWISER</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>WORLDWISER</h1>
+      {/* reloading
+      <a href="/pricing">Pricing</a> */}
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default Homepage;
