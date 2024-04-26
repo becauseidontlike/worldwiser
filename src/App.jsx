e54a04d664642fb98d086b2959a38045e563bfd6
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-26 22:43:16 +0200

    fake server with cities data, fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,31 @@ import Pagenotfound from "./pages/Pagenotfound";
 import AppLayout from "./pages/AppLayout";
 import "./index.css";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const base_url = "http://localhost:9000/cities";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${base_url}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("there was an error...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +39,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>TEST List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>FORM</p>} />
         </Route>
