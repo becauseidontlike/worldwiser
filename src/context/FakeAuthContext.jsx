a389f03be86ef824d17f0d0586dc11fcd1fb4360
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-05-10 22:15:28 +0200

    reducer, next steps

--- src/context/FakeAuthContext.jsx
+++ src/context/FakeAuthContext.jsx
@@ -1,8 +1,32 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
 function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducerl,
+    initialState
+  );
+  function login(email, password) {}
+
+  function logout() {}
+
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
 }
 
