94ee6c28adabfad4fd7d5e8790dd1a6ab9204741
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-05-11 21:06:25 +0200

    navigation to app after succesfull login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,29 @@
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
 import Button from "../components/Button";
 import { useAuth } from "../context/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
-  const { login } = useAuth();
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (email && password) login(email, password);
   }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app");
+    },
+    [isAuthenticated, navigate]
+  );
 
   return (
     <main className={styles.login}>
